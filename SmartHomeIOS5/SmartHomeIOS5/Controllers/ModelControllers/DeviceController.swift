5fe6f0a1666194710d4044eb9281b91dfa8ffdb7
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-07 13:27:13 -0600

    finished modelController and made the model codable

--- SmartHomeIOS5/SmartHomeIOS5/Controllers/ModelControllers/DeviceController.swift
+++ SmartHomeIOS5/SmartHomeIOS5/Controllers/ModelControllers/DeviceController.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+class DeviceController {
+    
+    // MARK: - Properties
+    static let sharedInstance = DeviceController()
+    var devices: [Device] = []
+    
+    init() {
+      load()
+    }
+    
+    // MARK: - CRUD Functions
+    func createDevice(name: String) {
+        let device = Device(name: name)
+        devices.append(device)
+        save()
+    }
+    
+    func deleteDevice() {
+        
+    }
+    
+    func switchIsOn(device: Device) {
+        device.isOn.toggle()
+        save()
+    }
+    
+    // MARK: - Persistence
+    func save() {
+        guard let url = fileURL else { return }
+        do {
+            let data = try JSONEncoder().encode(devices)
+            try data.write(to: url)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func load() {
+        guard let url = fileURL else { return }
+        do {
+            let data = try Data(contentsOf: url)
+            let devices = try JSONDecoder().decode([Device].self, from: data)
+            self.devices = devices
+        } catch {
+            print(error)
+        }
+    }
+    
+    private var fileURL: URL? {
+        guard let documentsDirector = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        let url = documentsDirector.appendingPathComponent("devices.json")
+        return url
+    }
+} //End of class
